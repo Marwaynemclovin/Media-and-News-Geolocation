ce45b0ca14a10eb06f31e53bafb5732d93d2c41f
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-01-03 19:01:04 +1100

    update to html and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var weatherEl = document.getElementById("weatherCard")
+
 function getWeatherApi() {
     var requestCoords = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=c73b072cff0fd87e69368bffee7e4662`;
     var weatherData = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${c73b072cff0fd87e69368bffee7e4662}`
@@ -12,9 +14,16 @@ function getWeatherApi() {
         var lon = data.coords.lon;
         secondFetch(lat, lon);
         for (var i = 0; i < data.length; i++) {
-          var listItem = document.createElement('li');
-          listItem.textContent = data[i].html_url;
-        //   fix this: repoList.appendChild(listItem);
+         weatherEl.innerHTML = `
+         <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">CITY NAME</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>`
         }
       });
   }
@@ -27,8 +36,6 @@ function getWeatherApi() {
         })
         .then(function (data){
             console.log(lat, lon);
-            // write code to put info into card in widget
         })
   }
   
-//   fetchButton.addEventListener('click', getApi);
