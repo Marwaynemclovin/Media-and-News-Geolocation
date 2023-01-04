fdb9b09e835ff2cf7da83ade426c2076dc670a22
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-01-04 12:29:15 +1100

    fixed API fetch in logic and templating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,123 +1,42 @@
+var mainWeatherDiv = document.getElementById('mainWeatherDiv');
 
-var weatherElCity1 = document.getElementById("weatherCard1")
-var weatherElCity2 = document.getElementById("weatherCard2")
-var weatherElCity3 = document.getElementById("weatherCard3")
-var weatherElCity4 = document.getElementById("weatherCard4")
-var weatherElCity5 = document.getElementById("weatherCard5")
+const apiKey = "c73b072cff0fd87e69368bffee7e4662";
+var fiveCities = ['Sydney', 'Adelaide', 'Melbourne', 'Brisbane', 'Perth'];
 
 function getWeatherApi() {
-  var requestCoordsSyd = `https://api.openweathermap.org/geo/1.0/direct?q=Sydney&limit=5&appid=c73b072cff0fd87e69368bffee7e4662`;
-  var requestCoordsAde = `https://api.openweathermap.org/geo/1.0/direct?q=Adelaide&limit=5&appid=c73b072cff0fd87e69368bffee7e4662`;
-  var requestCoordsMel = `https://api.openweathermap.org/geo/1.0/direct?q=Melbourne&limit=5&appid=c73b072cff0fd87e69368bffee7e4662`;
-  var requestCoordsBri = `https://api.openweathermap.org/geo/1.0/direct?q=Brisbane&limit=5&appid=c73b072cff0fd87e69368bffee7e4662`;
-  var requestCoordsPer = `https://api.openweathermap.org/geo/1.0/direct?q=Perth&limit=5&appid=c73b072cff0fd87e69368bffee7e4662`;
-  fetch(requestCoordsSyd)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      secondFetch(lat, lon);
-      var lat = data.coords.lat;
-      var lon = data.coords.lon;
-      for (var i = 0; i < data.length; i++) {
-        weatherElCity1.innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">CITY NAME</h5>
-            <p class="card-text" id= "temp">TEMP</p>
-            <p class="card-text">forecast img</p>
-            <p class="card-text">forecast text</p>
-          </div>
-        </div>`
-    }});
-  fetch(requestCoordsAde)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      secondFetch(lat, lon);
-      var lat = data.coords.lat;
-      var lon = data.coords.lon;
-      for (var i = 0; i < data.length; i++) {
-        weatherElCity2.innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">CITY NAME</h5>
-            <p class="card-text" id= "temp">TEMP</p>
-            <p class="card-text">forecast img</p>
-            <p class="card-text">forecast text</p>
-          </div>
-        </div>`
-      }});
-  fetch(requestCoordsMel)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      secondFetch(lat, lon);
-      var lat = data.coords.lat;
-      var lon = data.coords.lon;
-      for (var i = 0; i < data.length; i++) {
-        weatherElCity3.innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">CITY NAME</h5>
-            <p class="card-text" id= "temp">TEMP</p>
-            <p class="card-text">forecast img</p>
-            <p class="card-text">forecast text</p>
-          </div>
-        </div>`
-      }});
-  fetch(requestCoordsBri)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      secondFetch(lat, lon);
-      var lat = data.coords.lat;
-      var lon = data.coords.lon;
-      for (var i = 0; i < data.length; i++) {
-        weatherElCity4.innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">CITY NAME</h5>
-            <p class="card-text" id= "temp">TEMP</p>
-            <p class="card-text">forecast img</p>
-            <p class="card-text">forecast text</p>
-          </div>
-        </div>`
-      }});
-  fetch(requestCoordsMel)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      secondFetch(lat, lon);
-      var lat = data.coords.lat;
-      var lon = data.coords.lon;
-      for (var i = 0; i < data.length; i++) {
-        weatherElCity5.innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">CITY NAME</h5>
-            <p class="card-text" id= "temp">TEMP</p>
-            <p class="card-text">forecast img</p>
-            <p class="card-text">forecast text</p>
-          </div>
-        </div>`
-      }});
-}
+  
+  fiveCities.forEach((city) => {
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=c73b072cff0fd87e69368bffee7e4662`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        secondFetch(lat,lon);
+      })
+  })
+};
   
 function secondFetch(lat, lon) {
-  var weatherData = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${c73b072cff0fd87e69368bffee7e4662}`
-  //second fetch 
+  var weatherData = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
   fetch(weatherData)
       .then(function(response){
           return response.json();
       })
       .then(function (data){
-          console.log(lat, lon);
+          console.log(data);
+          var weatherElDiv = document.createElement('div');
+                  weatherElDiv.innerHTML = `
+                  <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">${data.city.name}</h5>
+                      <img src='https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png' alt=${data.list[0].weather.description}''>
+                      <p class="card-text">Currently in ${data.city.name}, it feels like ${data.list[0].main.feels_like}</p>
+                    </div>
+                  </div>`;
+                  mainWeatherDiv.append(weatherElDiv);
       })
-}
+};
   
 getWeatherApi();
